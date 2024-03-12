044073970be942c40764835904a475d4f9dd17e3
Winnie <winiigboama@gmail.com>
2024-03-12 01:33:15 +0100

    chore: rename paystack_di as processor_di
    use generic name processor_di in project instead of paystack_di.

--- processor_di/wsgi.py
+++ processor_di/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for paystack_di project.
+WSGI config for processor_di project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paystack_di.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'processor_di.settings')
 
 application = get_wsgi_application()
